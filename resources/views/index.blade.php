c0698189aa06198a3a451fc72757585d10874dfb
Nicolas Carpita <nicolascarpita@Nicolass-MacBook-Pro.local>
2024-11-14 15:15:56 +0100

    first commitv2

---
+++ resources/views/index.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dans ton fiac</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="relative h-screen w-screen bg-gray-50">
+    <header class="bg-slate-700 h-20 fixed top-0 left-0 w-full border-b-2 border-black">
+        <nav class="flex items-center gap-10">
+            <img src="https://cdn.discordapp.com/attachments/1159414220572602458/1306617419145547858/Capture_decran_2024-11-14_144919.png?ex=673751cc&is=6736004c&hm=e73695561f80fdb65205f501fc2fb3dd68316b3338a8b0b9a828637783de0b2e&" alt="" class="h-full">
+            <ul class="flex gap-6 items-center flex-nowrap">
+                <li class="text-xl font-semibold text-zinc-100 hover:text-white transition-all ease-in-out duration-200">Accueil</li>
+                <li class="text-xl font-semibold text-zinc-100 hover:text-white transition-all ease-in-out duration-200">Random Fiak</li>
+                <li class="text-xl font-semibold text-zinc-100 hover:text-white transition-all ease-in-out duration-200">Fiak du jour</li>
+            </ul>
+
+        </nav>
+    </header>
+
+    <main>
+        <div class="w-full flex flex-col">
+            <h1 class="text-3xl mt-24 font-bold w-full text-center">Bienvenue sur le site qui t'envoi tout droit dans ton fiak !</h1>
+            <p class="text-gray-400 text-xl text-center">Un site qui a une raie d'avance</p>
+        </div>
+
+    </main>
+
+    <footer>
+
+    </footer>
+    
+</body>
+</html>
